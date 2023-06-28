b8aab9dd7e6ad5de00cdbe17bfc8756e16262af7
Darian Hiebl <dahiebl@gmail.com>
2023-06-28 07:16:36 -0300

    Agregados modelos genero y articulo

--- apps/articulo/admin.py
+++ apps/articulo/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from apps.articulo.models import Articulo, Genero
+
 # Register your models here.
+
+
+class GeneroModel(admin.ModelAdmin):
+    list_display = ('nombre', 'padre')
+    
+
+admin.site.register(Articulo)
+admin.site.register(Genero, GeneroModel)
--- apps/articulo/models.py
+++ apps/articulo/models.py
@@ -1,12 +1,36 @@
-from collections import UserList
+from time import strftime
 from django.db import models
-from django.contrib.auth.models import User
 
 # Create your models here.
-class Articulo(models.Model):
+
+class Genero(models.Model):
+    nombre = models.CharField(
+        max_length=35, 
+        verbose_name="Genero", 
+        help_text="Nombre del genero"
+        )
+    padre = models.ForeignKey(
+        'self', 
+        related_name = 'Genero_padre',
+        on_delete = models.CASCADE,
+        null = True,
+        blank=True
+        )
     
+    def __str__(self) -> str:
+        return str(self.nombre)
+
+
+class Articulo(models.Model):
     fecha = models.DateField(auto_now = True, verbose_name = "Fecha de publicacion")
     titulo = models.CharField(max_length=100, verbose_name="Titulo", help_text="Ingrese el titulo del articulo")
     resumen = models.TextField(verbose_name= 'Resumen', help_text='Ingrese aqui su resumen')
     contenido = models.TextField(verbose_name= "Contenido", help_text="Ingrese aqui el contenido del articulo")
-    usuario = models.ForeignKey(User, on_delete = models.SET_NULL, null=True, verbose_name = 'Autor')
+    genero = models.ForeignKey(Genero, on_delete = models.CASCADE, null=True, verbose_name="Genero", help_text="Ingrese el genero")
+    
+    
+    
+    def __str__(self) -> str:
+        return str(self.titulo + " " + self.fecha.strftime("%d-%m-%Y"))
+    
+
